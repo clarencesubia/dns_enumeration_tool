9e5d87007dfbbfeb9652836d9d9826928aaa4b25
Clarence Subia <72455856+meliodaaf@users.noreply.github.com>
2022-01-29 23:03:08 +0800

    Update dnslookup.py

--- dnslookup.py
+++ dnslookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import dns.resolver
+import dns.reversename
 import argparse
 
 
@@ -17,7 +18,7 @@ record_type = args.query
 target_file = args.file
 
 if not record_type:
-    record_type = ["A", "AAAA", "NS", "CNAME", "MX", "PTR", "SOA", "TXT"]
+    record_type = ["A", "AAAA", "NS", "CNAME", "MX", "SOA", "TXT"]
 
 def main():
     if target_file:
@@ -28,29 +29,31 @@ def main():
         query(target, record_type)
 
 def query(target, records):
-    print("\n" + ' Starting DNS query for ' + str(target))
+    print("\n" + 'Starting DNS query for ' + str(target))
     for record in records:
         query_record(target, record)
 
 
 def query_record(host, record):
     try:
-        answer = dns.resolver.resolve(host, record.upper())
-        print(f"\n{record.upper()} Records:")
-        print("-" * 30)
-        for server in answer:
-            print(server.to_text())
+        if "ptr" in record_type:
+            qname = dns.reversename.from_address(host)
+            n = dns.resolver.resolve(qname, 'PTR')
+            for server in n:
+                print(server)
+        else:
+            answer = dns.resolver.resolve(host, record.upper())
+            print(f"\n{record.upper()} Records:")
+            print("-" * 30)
+            for server in answer:
+                print(server.to_text())
     except dns.resolver.NoAnswer:
         pass
     except dns.resolver.NXDOMAIN:
-        print(f"The DNS query name does not exist: {host}")
+        print(f"The DNS name does not exist: {host}")
         quit()
     except KeyboardInterrupt:
         quit()
 
 
 if __name__ == '__main__': main()
-
-
-
-
